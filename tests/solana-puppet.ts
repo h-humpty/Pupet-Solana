73d2f766b73efa6a72f5b332c199f0873c8a5559
passionatedeveloper223 <strongwolf223@gmail.com>
2022-04-01 07:08:40 -0700

    finish test using mocha and chai

--- tests/solana-puppet.ts
+++ tests/solana-puppet.ts
@@ -1,16 +1,41 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { Keypair, SystemProgram } from '@solana/web3.js';
+import { expect } from 'chai';
+
 import { SolanaPuppet } from "../target/types/solana_puppet";
+import { PuppetShadow } from "../target/types/puppet_shadow";
 
 describe("solana-puppet", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.SolanaPuppet as Program<SolanaPuppet>;
+  const puppetProgram = anchor.workspace.SolanaPuppet as Program<SolanaPuppet>;
+  const puppetShadowProgram = anchor.workspace.PuppetShadow as Program<PuppetShadow>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  const puppetKeypair = Keypair.generate();
+
+  it('Does CPI!', async () => {
+    await puppetProgram.rpc.initialize({
+      accounts: {
+        puppet: puppetKeypair.publicKey,
+        user: anchor.getProvider().wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [puppetKeypair]
+    });
+
+    await puppetShadowProgram.rpc.pullStrings(new anchor.BN(42), {
+      accounts: {
+        puppetProgram: puppetProgram.programId,
+        puppet: puppetKeypair.publicKey
+      }
+    });
+
+    expect((
+      await puppetProgram.account.data.fetch(
+        puppetKeypair.publicKey
+      )
+    ).data.toNumber()).to.equal(42);
   });
 });
